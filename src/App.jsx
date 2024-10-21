8a927759ac308c37aa44dc422647e11f367bef1b
Chineta Adinnu <chinetaadinnu@gmail.com>
2024-10-21 19:24:17 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 
 import Layout from './components/layout/Layout';
 const About = lazy(() => import('./pages/about/About.jsx'));
@@ -6,11 +6,6 @@ const Banner = lazy(() => import('./pages/banner/Banner.jsx'));
 const Contact = lazy(() => import('./pages/contact/Contact.jsx'));
 const Hero = lazy(() => import('./pages/hero/Hero.jsx'));
 const Companies = lazy(() => import('./pages/solutions/Companies.jsx'));
-
-// const Home = lazy(() => import('./pages/home/Home.jsx'));
-// const Signup = lazy(() => import('./pages/auth/Signup.jsx'));
-// const Login = lazy(() => import('./pages/auth/Login.jsx'));
-// const Dashboard = lazy(() => import('./pages/dashboard/Dashboard.jsx'));
 
 const App = () => {
   return (
